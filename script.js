394f8f906603a3fc31b8a7568629bffd16ef9c60
Deva_kumar.N <83574761+Devakumar96@users.noreply.github.com>
2022-12-07 15:23:24 +0530

    gallery setup

---
+++ script.js
@@ -0,0 +1,45 @@
+
+  
+//   javascript
+  
+  var small_images  = document.getElementsByClassName("small-image");
+  var show_image_popup  = document.getElementById("show_image_popup");
+  var large_image  = document.getElementById("large-image");
+  var close_btn = document.getElementById("close-btn");
+  
+  for(var i=0; i< small_images.length; i++){
+  
+    small_images[i].addEventListener("click", function(){
+      // remove active class from every images
+      for(var j=0; j< small_images.length; j++){
+        small_images[j].classList.remove('active');
+      }
+      // end
+   
+      this.classList.add('active'); // add active class to this image
+      
+      var src_val = this.src;
+      large_image.src = src_val;
+      showModal();
+    });
+  }
+  
+  
+  function showModal(){
+    show_image_popup.style.display = 'block';
+  }
+  
+  close_btn.addEventListener("click", function(){
+    // before colose the modal we need to remove active class
+    for(var i=0; i< small_images.length; i++){
+      small_images[i].classList.remove('active');
+    }
+    // end
+    closeModal();
+  });
+  
+  function closeModal(){
+    show_image_popup.style.display = 'none';
+  }
+  
+  
